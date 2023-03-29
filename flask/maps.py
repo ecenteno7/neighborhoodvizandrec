d6f60daee3ede607eab5b8707e014e58b4f607e9
rchomko <40405213+rchomko@users.noreply.github.com>
2023-03-29 00:15:32 -0400

    removed public maps key

--- flask/maps.py
+++ flask/maps.py
@@ -2,10 +2,11 @@ import os
 import googlemaps
 import json
 import neighborhood_methods as nm
+from shapely.geometry import Point, shape, Polygon
 
 
-def get_gmaps_info(lat, lon, city, neighborhoods_file):
-    gmaps = googlemaps.Client(key="AIzaSyAIBH2mzDjd-OGXEi4S50MmVNQw8EFyEnE")
+def get_gmaps_info(apikey, lat, lon, city, neighborhoods_file):
+    gmaps = googlemaps.Client(key=apikey)
     center_coords = (lat, lon)
     
     with open(neighborhoods_file) as f:
@@ -15,25 +16,44 @@ def get_gmaps_info(lat, lon, city, neighborhoods_file):
 
     search_types = ["restaurant", "cafe", "meal_delivery", "meal_takeaway"]
     restaurant_results = {'results': []}
+    
+    place_types = ["accounting", "airport", "amusement_park", "aquarium", "art_gallery", "atm", "bakery", "bank", "bar", "beauty_salon", "bicycle_store", "book_store", "bowling_alley", "bus_station", "cafe", "campground", "car_dealer", "car_rental", "car_repair", "car_wash", "casino", "cemetery", "church", "city_hall", "clothing_store", "convenience_store", "courthouse", "dentist", "department_store", "doctor", "drugstore", "electrician", "electronics_store", "embassy", "fire_station", "florist", "funeral_home", "furniture_store", "gas_station", "gym", "hair_care", "hardware_store", "hindu_temple", "home_goods_store", "hospital", "insurance_agency", "jewelry_store", "laundry", "lawyer", "library", "light_rail_station", "liquor_store", "local_government_office", "locksmith", "lodging", "meal_delivery", "meal_takeaway", "mosque", "movie_rental", "movie_theater", "moving_company", "museum", "night_club", "painter", "park", "parking", "pet_store", "pharmacy", "physiotherapist", "plumber", "police", "post_office", "primary_school", "real_estate_agency", "restaurant", "roofing_contractor", "rv_park", "school", "secondary_school", "shoe_store", "shopping_mall", "spa", "stadium", "storage", "store", "subway_station", "supermarket", "synagogue", "taxi_stand", "tourist_attraction", "train_station", "transit_station", "travel_agency", "university", "veterinary_care", "zoo"]
 
-    for search_type in search_types:
-        search_results = gmaps.places_nearby(location=(lat, lon), radius=5000, type=search_type)
+    for search_type in place_types:
+        search_results = gmaps.places_nearby(location=(lat, lon), radius=100, type=search_type)
         restaurant_results['results'].extend(search_results['results'])
 
-    for place in restaurant_results['results']:
-        print(place['name'])
-        print(nm.find_neighborhood(place['geometry']['location']['lat'], place['geometry']['location']['lng'], city))
-        print()
+    filtered_results = []
     
+    # for place in restaurant_results['results']:
+    #     print(place['name'])
+    #     print(nm.find_neighborhood(place['geometry']['location']['lat'], place['geometry']['location']['lng'], city))
+    #     print()
+
+ 
     print("Restaurant/Cafe/Takeaway/Delivery Count:", len(restaurant_results['results']))
     #determine if restaurant lat/lon is within the a particular neighborhood polygon, if not, then remove from list
     #Count # of restaurants
+    # for place in restaurant_results['results']:
+    #     print(place['name'])
+    #     print(nm.find_neighborhood(place['geometry']['location']['lat'], place['geometry']['location']['lng'], city))
+    #     print()
+
+    #     point = Point(place['geometry']['location']['lng'], place['geometry']['location']['lat'])
+    #     for feature in neighborhoods['features']:
+    #         polygon = shape(feature['geometry'])
+    #         if polygon.contains(point):
+    #             filtered_results.append(place)
+    #             # break
 
     return restaurant_results
 
-    
+with open('../googleMapsApikey.txt') as f:
+    api_key = f.readline()
+    f.close   
 
-results = get_gmaps_info(40.75834719172825, -73.98543110503996, "nyc", "flask/neighborhoods/nyc.geojson")
+print(api_key)
+results = get_gmaps_info(api_key, 40.75834719172825, -73.98543110503996, "nyc", "flask/neighborhoods/nyc.geojson")
 
 
 
