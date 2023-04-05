d90f123d5913288974428e98d2f3a8d3284af828
Erik Centeno <erikcenteno77@gmail.com>
2023-04-04 22:11:21 -0400

    Added preference rendering on map2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import * as React from 'react';
-import MapComponent from './components/MapComponent';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import atlanta_geojson from './resources/atlanta.geojson';
-import chicago_geojson from './resources/chicago.geojson';
+import * as React from "react";
+import MapComponent from "./components/MapComponent";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import atlanta_geojson from "./resources/atlanta.json";
+import chicago_geojson from "./resources/chicago.json";
+import PreferencesComponent from "./components/PreferencesComponent";
 
 function App() {
-  const [selectedMap, setSelectedMap] = React.useState('map1');
+  const [selectedMap, setSelectedMap] = React.useState("map1");
 
   const handleMapSelection = (event, newSelectedMap) => {
     setSelectedMap(newSelectedMap);
-    if (selectedMap === 'map1') {
-      selectedGeoJson = atlanta_geojson;
-      selectedInitialViewState = {
-        longitude: -84.376656, 
-        latitude: 33.749542, 
-        zoom: 10
-      };
-    } else {
-      selectedGeoJson = chicago_geojson;
-      selectedInitialViewState = {
-        longitude: 87.629798, 
-        latitude: 41.878114, 
-        zoom: 10
-      };
-    }
   };
 
   return (
@@ -33,7 +19,7 @@ function App() {
         value={selectedMap}
         exclusive
         onChange={handleMapSelection}
-        style={{width: 500, height: 25, margin: 'auto', padding: 20}}
+        style={{ width: 500, height: 25, margin: "auto", padding: 20 }}
       >
         <ToggleButton value="map1" aria-label="left aligned">
           <p>Map 1</p>
@@ -42,22 +28,29 @@ function App() {
           <p>Map 2</p>
         </ToggleButton>
       </ToggleButtonGroup>
-      {selectedMap == 'map1' && <MapComponent 
-        geojson={atlanta_geojson} 
-        initialViewState={{
-          longitude: -84.376656, 
-          latitude: 33.749542, 
-          zoom: 10
-        }}>
-      </MapComponent>}
-      {selectedMap == 'map2' && <MapComponent 
-        geojson={chicago_geojson} 
-        initialViewState={{
-          longitude: -87.63238, 
-          latitude: 41.84372, 
-          zoom: 9
-        }}>
-      </MapComponent>}
+      {selectedMap == "map1" && (
+        <MapComponent
+          geojson={atlanta_geojson}
+          initialViewState={{
+            longitude: -84.376656,
+            latitude: 33.749542,
+            zoom: 10,
+          }}
+        ></MapComponent>
+      )}
+      {selectedMap == "map2" && (
+        <>
+          <MapComponent
+            geojson={chicago_geojson}
+            initialViewState={{
+              longitude: -87.63238,
+              latitude: 41.84372,
+              zoom: 9,
+            }}
+          ></MapComponent>
+          <PreferencesComponent></PreferencesComponent>
+        </>
+      )}
     </div>
   );
 }
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,34 +1,75 @@
-import React, { Component } from 'react'
-import Map, {Source, Layer} from 'react-map-gl';
+import React, { Component } from "react";
+import Map, { Source, Layer } from "react-map-gl";
 
 class MapComponent extends Component {
-    constructor(props){
-        super(props)
-    }
-      
-    layerStyle = {
-        id: 'point',
-        type: 'fill',
-        paint: {
-          "fill-color": "red",
-          "fill-opacity": 0.2
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      res: {
+        neighborhoods: [
+          {
+            key: "West Midtown",
+            description:
+              "West Midtown, also known as Westside,[1] is a colloquial area, comprising many historical neighborhoods located in Atlanta, Georgia. Once largely industrial, West Midtown is now the location of urban lofts, art galleries, live music venues, retail and restaurants.",
+            pointsOfInterest: [
+              "Ormsby's",
+              "Puttshack",
+              "Fire Maker Brewing Company",
+            ],
+          },
+          {
+            key: "East Atlanta",
+            description:
+              "East Atlanta is a neighborhood on the east side of Atlanta, Georgia, United States. The name East Atlanta Village primarily refers to the neighborhood's commercial district.",
+            pointsOfInterest: ["Ramen Place", "Coffee Shop", "Brewery"],
+          },
+          {
+            key: "Candler Park",
+            description:
+              "Leafy Candler Park is a quiet, largely residential neighborhood, with late-19th- and early-20th-century homes. Community events are often held at landscaped Candler Park, which has a golf course, playground and swimming pool. Trails wind past outdoor art installations in adjacent Freedom Park. McLendon Avenue is home to laid-back cafes and restaurants that serve Southern food.",
+            pointsOfInterest: ["Coffee", "Bar", "Movie Theater"],
+          },
+        ],
+      },
     };
-      
-    render() {
-        return (
-            <Map
-                mapboxAccessToken={process.env.REACT_APP_API_KEY}
-                initialViewState = {this.props.initialViewState}
-                style={{width: 600, height: 400, margin: 'auto', padding: 20}}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-            >
-                <Source id="my-data" type="geojson" data={this.props.geojson}>
-                    <Layer {...this.layerStyle} />
-                </Source>
-            </Map>
-        );
-    }
+    let selectedNeighborhoods = [];
+  }
+
+  componentDidMount(prevProps) {
+    const prefNeighborhoods = this.state.res.neighborhoods.map((interest) => {
+      return interest.name;
+    });
+    this.props.geojson.features.map((feature) => {
+      if (prefNeighborhoods.includes(feature.properties.name)) {
+        this.state.selectedNeighborhoods.push(feature);
+      }
+    });
+    console.log(this.state.selectedNeighborhoods);
+  }
+
+  layerStyle = {
+    id: "point",
+    type: "fill",
+    paint: {
+      "fill-color": "red",
+      "fill-opacity": 0.2,
+    },
+  };
+
+  render() {
+    return (
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_API_KEY}
+        initialViewState={this.props.initialViewState}
+        style={{ width: 600, height: 400, margin: "auto", padding: 20 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Source id="my-data" type="geojson" data={this.props.geojson}>
+          <Layer {...this.layerStyle} />
+        </Source>
+      </Map>
+    );
+  }
 }
 
 export default MapComponent;
