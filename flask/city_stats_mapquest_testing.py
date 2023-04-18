96de7daa3a3bed393445ba4a8c00d486dce65c4d
rchomko <40405213+rchomko@users.noreply.github.com>
2023-04-17 20:57:35 -0400

    DC neighborhood files updated

--- flask/city_stats_mapquest_testing.py
+++ flask/city_stats_mapquest_testing.py
@@ -9,13 +9,15 @@ with open('../mapquestAPIkey.txt') as f:
 
 #list_neighborhoods("washingtondc")
 
+# city_poi_list_mapquest(mq_api_key, "chicago")
 # city_poi_list_mapquest(mq_api_key, "washingtondc")
+
 
 # remove_poi_duplicates("flask/neighborhoods/chicago_places_of_interest.csv")
 # remove_poi_duplicates("flask/neighborhoods/washingtondc_places_of_interest.csv")
 
 # id_poi_with_neighborhood("flask/neighborhoods/chicago_places_of_interest.csv", "chicago")
-# id_poi_with_neighborhood("flask/neighborhoods/chicago_places_of_interest.csv", "washingtondc")
+# id_poi_with_neighborhood("flask/neighborhoods/washingtondc_places_of_interest.csv", "washingtondc")
 
 # tally_occurrences("flask/neighborhoods/chicago_poi_with_neighborhood.csv", "chicago")
-# tally_occurrences("flask/neighborhoods/chicago_poi_with_neighborhood.csv", "washingtondc")
+tally_occurrences("flask/neighborhoods/washingtondc_poi_with_neighborhood.csv", "washingtondc")
